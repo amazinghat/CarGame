e93f8dab9efaef6b2f191232b17e2be4ae383608
unknown <olek.gawin@gmail.com>
2018-05-07 21:18:36 +0200

    Autko bandytow

---
+++ Assets/Scripts/BanditCarBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BanditCarBehaviour : MonoBehaviour {
+
+	public GameObject bomb;
+	public int bombsAmount;
+	public int banditCarVerticalSpeed;
+	public int banditCarHorizontalSpeed;
+	public float bombDelay;
+
+	private float delay;
+	private GameObject playerCar;
+	private Vector3 banditCarPos;
+
+	void Start()
+	{
+		playerCar = GameObject.FindWithTag ("Player");
+		delay = bombDelay;
+	}
+
+	void Update()
+	{
+		if (playerCar == null) 	// sprawdzaj czy auto gracza istnieje
+		{
+			playerCar = GameObject.FindWithTag ("Player");
+		} 
+		else 
+		{
+			if (gameObject.transform.position.y > 3.8f && bombsAmount > 0) {		// jesli zaczyna na gorze mapy i ma bomby
+				this.gameObject.transform.Translate (new Vector3 (0, -1, 0) * banditCarVerticalSpeed * Time.deltaTime);		// jedzie w dol
+
+			} else if (bombsAmount <= 0) {		// jesli skonczyly sie bomby
+				this.gameObject.transform.Translate (new Vector3 (0, 1, 0) * banditCarVerticalSpeed * Time.deltaTime);		// wraca na gore
+				if (gameObject.transform.position.y > 6.5f) 
+				{
+					Destroy (this.gameObject);		// i znika
+				}
+			}
+			else 
+			{
+				banditCarPos = Vector3.Lerp (transform.position, playerCar.transform.position, Time.fixedDeltaTime * banditCarHorizontalSpeed);
+				transform.position = new Vector3 (banditCarPos.x, transform.position.y, 0);
+
+				delay -= Time.deltaTime;
+				if (delay <= 0 && bombsAmount > 5) {
+					delay = bombDelay;
+					bombsAmount--;
+					Instantiate (bomb, transform.position, Quaternion.identity);
+				} else if (delay <= 0 && bombsAmount <= 5 && bombsAmount > 0) 
+				{
+					delay = bombDelay / 2;
+					bombsAmount--;
+					Instantiate (bomb, transform.position, Quaternion.identity);
+				}
+			}
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+	public int bombDamage;
+	public float bombSpeed;
+
+	void Update(){
+		this.gameObject.transform.Translate (new Vector3 (0, -1, 0) * bombSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D obj) {
+		if (obj.gameObject.tag == "Player") {
+			obj.gameObject.GetComponent<PlayerCarMovement> ().durability -= bombDamage;
+			Destroy (this.gameObject);
+		} 
+		else if (obj.gameObject.tag == "Shield") {
+			Destroy (this.gameObject);
+		} 
+		else if (obj.gameObject.tag == "EndOfTheRoad") {
+			Destroy (this.gameObject);
+		}
+	}
+
+}
