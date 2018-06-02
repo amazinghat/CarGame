dc708c02cb4506826917216dd3db738fec2359a9
Klaudia Tomana <klaudia.tomana@gmail.com>
2018-06-02 18:46:14 +0200

    Add StartButton and ExitButton functionality and add music

--- Assets/Scripts/MenuFunctionality.cs
+++ Assets/Scripts/MenuFunctionality.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuFunctionality : MonoBehaviour {
 
@@ -22,5 +23,21 @@ public class MenuFunctionality : MonoBehaviour {
 			blueLight.enabled = !blueLight.enabled;
 			delay = lightDelay;
 		}
+	}
+
+	public void StartButton() {
+		SceneManager.LoadScene(0);
+	}
+
+	public void HighScoresButton() {
+		//SceneManager.LoadScene(2);
+	}
+
+	public void OptionsButton() {
+		//SceneManager.LoadScene(3);
+	}
+
+	public void ExitButton() {
+		Application.Quit();
 	}
 }
